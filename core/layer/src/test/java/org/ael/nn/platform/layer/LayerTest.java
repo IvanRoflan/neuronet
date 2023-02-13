438e0406d45369b52462c2dcf6db59b27f205cba
vaganovdv <vaganovdv@hs>
2023-02-13 20:58:38 +1000

    Исправление примера теста

--- core/layer/src/test/java/org/ael/nn/platform/layer/LayerTest.java
+++ core/layer/src/test/java/org/ael/nn/platform/layer/LayerTest.java
@@ -16,25 +16,23 @@ public class LayerTest {
     
     /**
      * Тест создания слоя нейронов
-    */
+     */
     @Test(priority = 1, groups = {"platform-core"})
     public void createLayer() {
 
         // Создается слой нейронов, который 
         // с номером 0, содержащий 1 нейрон        
         Layer layer = new Layer(0, 3);
-        
+
         // Создается вектор с разменостью 3 элемента
         Vector v1 = new Vector(4);
-        
+
         // Добавление вектора 
         List<Synapse> neuronInput = layer.addInputVector(v1);
-        
-        
-        
-        
+
+        // печать слоя на терминале
         System.out.println(layer);
-      
+
     }
 
     
