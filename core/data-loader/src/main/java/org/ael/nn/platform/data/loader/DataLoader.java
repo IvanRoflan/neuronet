b0a9c27a5285df4f0d57968b3d50b6463ce3b488
vaganovdv <vaganovdv@list.ru>
2023-02-28 10:06:22 +1000

    Чтение изображения из файла

--- core/data-loader/src/main/java/org/ael/nn/platform/data/loader/DataLoader.java
+++ core/data-loader/src/main/java/org/ael/nn/platform/data/loader/DataLoader.java
@@ -4,6 +4,9 @@
 
 package org.ael.nn.platform.data.loader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import org.ael.nn.platform.matrix.Matrix;
 
 
@@ -14,34 +17,62 @@ import org.ael.nn.platform.matrix.Matrix;
  */
 public class DataLoader {
 
+    // модификатор доступа      название класса      название экземпляра
+    //  |                        /                    /
+    private                    File                 file; 
     
-      /**
+    // Класс для синтаксического разбора 
+    private Scanner scanner;
+            
+            
+    /**
      * Загрузка изображения из файла
      *
      * @param path строковый путь к файлу
-     * @param fileName
+     * @param fileName каталог с файлом имя файла | /
      */
     public Matrix load(String path, String fileName) {
+
         Matrix m = new Matrix();
 
         System.out.println("Загрузка изображения из файла ...");
 
         if (path != null && !path.isEmpty()) {
-
             if (fileName != null && !fileName.isEmpty()) {
                 System.out.println("Начало загрузки ...");
-                System.out.println("Каталог загрузки: "+ path);
-                System.out.println("Файл: "+ fileName);
+                System.out.println(String.format("%-35s %s", "Каталог загрузки: ", path));
+                System.out.println(String.format("%-35s %s", "Файл: ", fileName));
+                String fullPath = path + File.separator + fileName;
+                file = new File(fullPath);
+                if (file.exists()) {
+                    System.out.println("Файл  [" + fileName + "] существует");
+                    try {
+                        scanner = new Scanner(file);
 
+                        // Читать файл построчно - разделитеть строк - символ перевода строки
+                        while (scanner.hasNextLine()) {
+                            String s = scanner.nextLine();
+                            System.out.println("Строка: " + s);
+                        }
+
+                    } catch (FileNotFoundException ex) {
+
+                        System.out.println("Ошибка ввода-вывода: " + ex.toString());
+                    }
+
+                } else {
+                    System.out.println("Ошибка: файл  [" + fileName + "] не существует");
+                }
             } else {
                 System.out.println("Загрузка изображения из файла не возможна: отсутствует указание имени файла");
             }
-
         } else {
             System.out.println("Загрузка изображения из файла не возможна: отсутствует путь к файлу");
         }
-
         return m;
-
     }
+
+
+    //public 
+    
 }
